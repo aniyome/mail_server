6bb209cbe31afe13028e0ef8b8d77beb2fe0bd4a
k.kumazawa <k.kumazawa@aveliouser-no-Mac-mini.local>
2017-04-14 13:00:44 +0900

    初期設定

---
+++ Vagrantfile
@@ -0,0 +1,15 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+  config.vm.provider :virtualbox do |provider, override|
+    override.vm.box = "centos7.2"
+
+    override.vm.hostname = "local.mail.service.ne.jp"
+    override.vm.network "private_network", ip: "192.168.33.55"
+
+    config.vbguest.auto_update = true
+  end
+
+  config.vm.synced_folder ".", "/vagrant"
+end
---
+++ provision/vm.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#----------------#
+#     config     #
+#----------------#
+
+# SELinuxを停止
+if [ `getenforce` != 'disable' ]; then
+    setenforce 0
+    sed -e "s/(enforcing|permissive)/disabled/g" /etc/selinux/config
+fi
+
+#-------------------#
+#     functions     #
+#-------------------#
+
+# 対象のプロセスが実行中か確認
+isProgressService () {
+    is_progress=`ps aux | grep -v grep | grep $1`
+    if [ ${#is_progress} -gt 0 ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#----------------------#
+#     provisioning     #
+#----------------------#
+
+##### yum update , install
+yum -y update
+yum install -y vim tree wget git postfix ntp
+
+##### iptables
+if isProgressService 'iptables'; then
+    service iptables stop
+    chkconfig iptables off
+fi
+
+##### ntp
+if ! isProgressService 'ntpd'; then
+    service ntpd start
+    chkconfig ntpd on
+fi
+
+##### expect
+yum -y install expect
+
+##### locate
+yum -y install mlocate
+updatedb
+
+##### postfix
+cp -a /vagrant/provision/postfix/main.cf /etc/postfix/main.cf
+service postfix restart
